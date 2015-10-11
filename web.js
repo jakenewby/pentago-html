53e2020df21e8aae6b3bc12e63d7b89b121252fd
= <jake@sticksnleaves.com>
2015-10-11 14:30:02 -0400

    rules routing

--- web.js
+++ web.js
@@ -4,4 +4,8 @@ var port = process.env.PORT || 5000;
 
 app.use(express.static('dist'));
 
+app.get('/rules', function(req, res) {
+  res.sendFile(__dirname + '/dist/rules.html');
+});
+
 app.listen(port);
