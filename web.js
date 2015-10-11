5f078f87154e11dc8bf0ad9194753b37d14eb541
= <jake@sticksnleaves.com>
2015-10-11 11:40:14 -0400

    fix node server

--- web.js
+++ web.js
@@ -2,6 +2,6 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 5000;
 
-app.use('/', express.static(__dirname + '/dist'));
+app.use(express.static('dist'));
 
 app.listen(port);
