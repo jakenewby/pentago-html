903c4e92a5d49beaf3becacd72d2a5e1ec53be51
= <jake@sticksnleaves.com>
2015-10-11 11:31:34 -0400

    update start config

--- web.js
+++ web.js
@@ -2,8 +2,6 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 5000;
 
-app.configure(function() {
-  app.use(express.static(path.join(__dirname, 'dist')));
-});
+app.use(express.static(__dirname + '/dist'));
 
 app.listen(port);
